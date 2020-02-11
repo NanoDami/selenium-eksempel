6c39009f5cb16573e0cf17edca60beaa1d59b966
Dan-Mikkel Hübenette <danmikkelhubenette@gmail.com>
2020-02-11 08:24:11 +0100

    La til id'er for å identifisere felter.

--- src/register/RegisterPage.vue
+++ src/register/RegisterPage.vue
@@ -5,12 +5,12 @@
             <div class="row">
                 <div class="form-group col-md-6">
                     <label htmlFor="firstName">First name</label>
-                    <input type="text" v-model="user.firstName" v-validate="'required'" name="firstName" class="form-control" :class="{ 'is-invalid': submitted && errors.has('firstName') }" />
+                    <input id="first_name_field" type="text" v-model="user.firstName" v-validate="'required'" name="firstName" class="form-control" :class="{ 'is-invalid': submitted && errors.has('firstName') }" />
                     <div v-if="submitted && errors.has('firstName')" class="invalid-feedback">{{ errors.first('firstName') }}</div>
                 </div>
                 <div class="form-group col-md-6">
                     <label htmlFor="lastName">Last name</label>
-                    <input type="text" v-model="user.lastName" v-validate="'required'" name="lastName" class="form-control" :class="{ 'is-invalid': submitted && errors.has('lastName') }" />
+                    <input id="last_name_field" type="text" v-model="user.lastName" v-validate="'required'" name="lastName" class="form-control" :class="{ 'is-invalid': submitted && errors.has('lastName') }" />
                     <div v-if="submitted && errors.has('lastName')" class="invalid-feedback">{{ errors.first('lastName') }}</div>
                 </div>
             </div>
@@ -18,13 +18,13 @@
             <div class="row">
                 <div class="form-group col-md-6">
                     <label htmlFor="username">Username</label>
-                    <input type="text" v-model="user.username" v-validate="{required: true, max: 6, min:6}" name="username" class="form-control" :class="{ 'is-invalid': submitted && errors.has('username') }" />
+                    <input id="username_field" type="text" v-model="user.username" v-validate="{required: true, max: 6, min:6}" name="username" class="form-control" :class="{ 'is-invalid': submitted && errors.has('username') }" />
                     <div v-if="submitted && errors.has('username')" class="invalid-feedback">{{ errors.first('username') }}</div>
                 </div>
 
                 <div class="form-group col-md-6">
                     <label htmlFor="password">Password</label>
-                    <input type="password" v-model="user.password" v-validate="{ required: true, min: 6 }" name="password" class="form-control" :class="{ 'is-invalid': submitted && errors.has('password') }" />
+                    <input id="password_field" type="password" v-model="user.password" v-validate="{ required: true, min: 6 }" name="password" class="form-control" :class="{ 'is-invalid': submitted && errors.has('password') }" />
                     <div v-if="submitted && errors.has('password')" class="invalid-feedback">{{ errors.first('password') }}</div>
                 </div>
             </div>
@@ -32,7 +32,8 @@
             <div class="row">
                 <div class="form-group col-md-6">
                     <label htmlFor="address">Address</label>
-                    <input type="text"
+                    <input id="address_field"
+                           type="text"
                            v-model="user.address"
                            v-validate="{ required: false}"
                            name="address"
@@ -43,7 +44,8 @@
 
                 <div class="form-group col-md-6">
                     <label htmlFor="phone">Phone</label>
-                    <input type="text"
+                    <input id="phone-field"
+                           type="text"
                            v-model="user.phone"
                            v-validate="{required: true}"
                            name="phone"
@@ -56,7 +58,8 @@
 
             <div class="form-group">
                 <label htmlFor="email">Email</label>
-                <input type="text"
+                <input id="email_field"
+                       type="text"
                        v-model="user.email"
                        v-validate="'required|email'"
                        name="email"
@@ -65,9 +68,8 @@
                 <div v-if="submitted && errors.has('email')" class="invalid-feedback">{{ errors.first('email') }}</div>
             </div>
 
-
             <div class="form-group">
-                <button class="btn btn-primary" :disabled="status.registering">Register</button>
+                <button id="register_button" class="btn btn-primary" :disabled="status.registering">Register</button>
                 <img v-show="status.registering" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
                 <router-link to="/login" class="btn btn-link">Cancel</router-link>
             </div>
