03d65e4e2dda750e8e1da4213bf32d6916df1135
Dan-Mikkel Hübenette <danmikkelhubenette@gmail.com>
2020-02-14 13:10:08 +0100

    Laget førsteutgave av testen vår.

---
+++ java-app/src/test/java/LogInTest.java
@@ -0,0 +1,9 @@
+public class LogInTest {
+/*
+    Når en person prøver å logge seg inn med feil brukernavn og/eller passord vi ikke har registrert på en bruker,
+    så vil vedkommende få en feilmelding.
+    -------------
+    1. Skriv inn et brukernavn og passord om ikke er registrert
+    2. Verifiser at man får en feilmelding
+ */
+}
